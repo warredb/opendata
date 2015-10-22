4d1c65002d1b84f1560aead1acab727f5e14c450
Ward <ward.debeuckelaer@hotmail.com>
2015-10-22 14:25:41 +0200

    uploading file

---
+++ Opendata/Opendata/JSON.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maps.MapControl.WPF;
+
+namespace Opendata
+{
+    public class JSON
+    {
+
+        public class Rootobject
+        {
+            public Paging paging { get; set; }
+            public Datum[] data { get; set; }
+        }
+
+        public class Paging
+        {
+            public int[] next { get; set; }
+            public int[] last { get; set; }
+            public int records { get; set; }
+            public int pages { get; set; }
+            public int pageCurrent { get; set; }
+            public int pageNext { get; set; }
+            public object pagePrev { get; set; }
+            public int pageSize { get; set; }
+        }
+
+        public class Datum
+        {
+            public string id { get; set; }
+            public string glascontainer_nr { get; set; }
+            public string point_lat { get; set; }
+            public string point_lng { get; set; }
+            public string straatnaam { get; set; }
+            public string huisnummer { get; set; }
+            public string postcode { get; set; }
+            public string type { get; set; }
+            public string ondergrond_verhard { get; set; }
+            public string opmerking { get; set; }
+            public string begindatum { get; set; }
+            public string objectid { get; set; }
+            public string gisid { get; set; }
+            public Location locatie { get; set; }
+        }
+    }
+}
---
+++ Opendata/Opendata/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Maps.MapControl.WPF;
+using Newtonsoft.Json;
+using HtmlAgilityPack;
+
+namespace Opendata
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SearchListBox.Items.Add("Straat");
+            SearchListBox.Items.Add("Postcode");
+            SearchListBox.Items.Add("Type");
+            SearchListBox.Items.Add("ID");
+        }
+
+        ObservableCollection<JSON.Datum> Glascontainers = new ObservableCollection<JSON.Datum>();
+        private IEnumerable<JSON.Datum> _linqRes;
+        private JSON.Rootobject data;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "http://datasets.antwerpen.be/v4/gis/glascontainer.json";
+
+            WebClient wc = new WebClient();
+            string jsondata = wc.DownloadString(url);
+
+            data = JsonConvert.DeserializeObject<JSON.Rootobject>(jsondata);
+            MessageBox.Show(data.data.Count() + " gevonden");
+            bool unique;
+
+            foreach (var datum in data.data)
+            {
+                unique = true;
+                for (int i = 0; i < Glascontainers.Count; i++)
+                {
+                    if (Glascontainers[i].id == datum.id && unique)
+                        unique = false;
+                }
+                if (unique)
+                {
+                    datum.locatie = new Location(Convert.ToDouble(datum.point_lat.Replace(".", ",")), Convert.ToDouble(datum.point_lng.Replace(".", ",")));
+                    Glascontainers.Add(datum);
+                }
+            }
+            GlascontainerListBox.ItemsSource = data.data;
+
+        }
+
+        private void GlascontainerListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (GlascontainerListBox.SelectedItem != null)
+            {
+                MapGrid.DataContext = GlascontainerListBox.SelectedItem;
+                JSON.Datum t = new JSON.Datum();
+                t = (JSON.Datum) GlascontainerListBox.SelectedItem;
+                leMap.Center = t.locatie;
+            }
+        }
+        
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+            
+            if (SearchListBox.SelectedItem == "Straat")
+            {
+                var glascontainers = from glascontainer in data.data
+                                     where glascontainer.straatnaam.Contains(SearchTxB.Text.ToUpper())
+                                     orderby glascontainer.straatnaam
+                                     select glascontainer;
+                MessageBox.Show(glascontainers.Count() + " gevonden");
+                GlascontainerListBox.ItemsSource = glascontainers;
+            }
+            if (SearchListBox.SelectedItem == "Postcode")
+            {
+                var glascontainers = from glascontainer in data.data
+                                     where glascontainer.postcode.Contains(SearchTxB.Text)
+                                     orderby glascontainer.straatnaam
+                                     select glascontainer;
+                MessageBox.Show(glascontainers.Count() + " gevonden");
+                GlascontainerListBox.ItemsSource = glascontainers;
+            }
+            if (SearchListBox.SelectedItem == "Type")
+            {
+                var glascontainers = from glascontainer in data.data
+                                     where glascontainer.type.Contains(SearchTxB.Text)
+                                     orderby glascontainer.straatnaam
+                                     select glascontainer;
+                MessageBox.Show(glascontainers.Count() + " gevonden");
+                GlascontainerListBox.ItemsSource = glascontainers;
+            }
+            }
+            catch (Exception Exc)
+            {
+                MessageBox.Show(Exc.Message);
+            }
+        }
+
+    }
+}
